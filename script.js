5a2cb976d328e3af7e2cedba4ddbd545602f0134
gllherme <guicampos159@gmail.com>
2021-10-24 01:20:03 -0300

    bug fix

--- script.js
+++ script.js
@@ -2,6 +2,37 @@ const calculator = {
     display: [],
 
     addNumber(number) {
-        this.display.push(number);
+        calculator.display.push(number);
+        console.log(calculator.display);
+    },
+
+    clearDisplay() {
+        calculator.display.length = 0;
+        console.log(calculator.display);
+    },
+
+    delete() {
+        calculator.display.pop();
+        console.log(calculator.display);
+    }
+
+}
+
+const DOM = {
+    displayContainer: document.querySelector(".calculator--display"),
+
+    showContent() {
+        let div = document.createElement('div');
+        
     }
 }
+
+
+var x = document.querySelector('.display--text') === null;
+
+let div = document.createElement('div')
+let text = document.createTextNode('TEXTO')
+div.appendChild(text);
+
+
+console.log(x);
