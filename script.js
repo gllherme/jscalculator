04191b50e0c7e97c148aa0bdf06746258bf542d0
gllherme <guicampos159@gmail.com>
2021-12-19 23:34:38 -0300

    basic operations working

--- script.js
+++ script.js
@@ -1,38 +1,105 @@
+let result = null;
+let operationSign = null;
+let memory = 0;
+
 const calculator = {
     display: [],
 
     addNumber(number) {
-        calculator.display.push(number);
-        console.log(calculator.display);
+        if(this.display.length < 16) {
+            this.display.push(number);
+            console.log(calculator.display);
+            calculator.updateDisplay();
+        } else {
+            return {}
+        }
     },
 
     clearDisplay() {
         calculator.display.length = 0;
+        calculator.updateDisplay();
         console.log(calculator.display);
     },
 
-    delete() {
-        calculator.display.pop();
-        console.log(calculator.display);
+    updateDisplay() {
+        const newDisplay = calculator.display.join("");
+        let content = document.querySelector('.display--text');
+        content.innerHTML = newDisplay;
+    },
+
+    updateMemory() {
+        if (this.display.length > 0) {
+            memory = parseInt(calculator.display.join(""));
+        } else {
+            memory = 0;
+        }
+    },
+
+    updateResult() {
+        result = parseInt(calculator.display.join(""));
+    },
+
+    clearMemory() {
+        memory = 0;
+    },
+
+    clearButton() {
+        this.clearDisplay()
+        this.clearMemory()
+    },
+
+    operation(sign) {
+        if (sign == "+") {
+            this.updateMemory();
+            this.clearDisplay();
+            operationSign = "+";
+
+        } else if (sign == "-") {
+            this.updateMemory();
+            this.clearDisplay();
+            operationSign = "-";
+
+        } else if (sign == "*") {
+            this.updateMemory();
+            this.clearDisplay();
+            operationSign = "*";
+
+        } else if (sign == "/") {
+            this.updateMemory();
+            this.clearDisplay();
+            operationSign = "/";
+        }
+    },
+
+    resultButton() {
+        if (operationSign == "+") {
+            let newDisplay = parseInt(this.display.join(""));
+            result = memory + newDisplay;
+            let strResult = result.toString();
+            calculator.display = strResult.split("");
+            this.updateDisplay();
+
+        } else if (operationSign == "-") {
+            let newDisplay = parseInt(this.display.join(""));
+            result = memory - newDisplay;
+            let strResult = result.toString();
+            calculator.display = strResult.split("");
+            this.updateDisplay();
+
+        } else if (operationSign == "*") {
+            let newDisplay = parseInt(this.display.join(""));
+            result = memory * newDisplay;
+            let strResult = result.toString();
+            calculator.display = strResult.split("");
+            this.updateDisplay();
+            
+        } else if (operationSign == "/") {
+            let newDisplay = parseInt(this.display.join(""));
+            result = memory / newDisplay;
+            let strResult = result.toString();
+            calculator.display = strResult.split("");
+            this.updateDisplay();
+        }
+
     }
-
 }
-
-const DOM = {
-    displayContainer: document.querySelector(".calculator--display"),
-
-    showContent() {
-        let div = document.createElement('div');
-        
-    }
-}
-
-
-var x = document.querySelector('.display--text') === null;
-
-let div = document.createElement('div')
-let text = document.createTextNode('TEXTO')
-div.appendChild(text);
-
-
-console.log(x);
